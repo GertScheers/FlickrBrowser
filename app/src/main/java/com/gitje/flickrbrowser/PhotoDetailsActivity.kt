6beef55d8bb81baa9ff8dcf0615931f2e0629665
Gitjeshub <38493098+Gitjeshub@users.noreply.github.com>
2020-11-07 16:51:40 +0100

    Load picture etc for details

--- app/src/main/java/com/gitje/flickrbrowser/PhotoDetailsActivity.kt
+++ app/src/main/java/com/gitje/flickrbrowser/PhotoDetailsActivity.kt
@@ -3,6 +3,8 @@ package com.gitje.flickrbrowser
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.content_photo_details.*
 
 class PhotoDetailsActivity : BaseActivity() {
 
@@ -10,6 +12,15 @@ class PhotoDetailsActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_details)
         activateToolbar(true)
+
+        val photo = intent.getSerializableExtra(PHOTO_TRANSFER) as Photo
+        photo_title.text = photo.title
+        photo_tags.text = photo.tags
+        photo_author.text = photo.author
+        Picasso.get().load(photo.link)
+            .error(R.drawable.placeholder)
+            .placeholder(R.drawable.placeholder)
+            .into(photo_image)
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
